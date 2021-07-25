14a2cf49bd93fbb8500d783ce47d73f6090342fd
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2021-07-25 10:37:20 -0400

    update

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
+function makeid() {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 4; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+  charactersLength));
+ }
+ return result;
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+colors = ["#423b0b","#b5fed9","#25283d","#dc851f","#20a4f3","#ee6352","#fac05e","#f79d84","#b08ea2","#368f8b"];
+
 class GameManager {
     constructor() {
       this.name = "greatestgauntlet"
+      this.peer = new Peer(makeid());
+      this.id = this.peer.id;
+      console.log(this.id);
+      this.players = [];
+      this.maxplayers = 10;
     }
     // Getter
-    async loadgame(gamefile) {
-        $.get( gamefile + "/index.html", function( data ) {
+    loadgame(gamefile) {
+      return new Promise(resolve =>
+
+        $.get( gamefile + "/index.html", {async: false}, function( data ) {
             $( ".game-content" ).html( data );
-            
-          });
-       
+            resolve();
+          }));
     }
   }
 class Player {
-    constructor(peer, name) {
-        this.peer = peer;
+    constructor(dataConnection, name) {
+        this.dataConnection = dataConnection;
         this.name = name;
+        this.tag;
+        this.points = 0;
+        this.color = '#2f2f2f';
+        this.id = this.dataConnection.peer;
+        this.vip = false;
+        
     }
 }
 async function gameloop() {
     game = new GameManager();
     await game.loadgame('start');
-    await execute();
+
+    await execute(game);
 
 }
 gameloop();
--- start/logic.js
+++ start/logic.js
@@ -1,3 +1,63 @@
-async function execute() {
 
+function checkvip(game) {
+  game.players[0].vip = true;
+  $.get( "start/screens/vip.html", function( data ) {
+    game.players[0].dataConnection.send({
+      cm: "contentChange",
+      content: data
+    });
+  });
+}
+
+async function execute(game) {
+    $('#rc').text(game.id);
+    game.peer.on('connection', function(conn) { 
+        if (game.players.length < game.maxplayers) {
+            p = new Player(conn, '');
+            p.tag = $('.player-area').append('<div class="player-icon" id="i' + p.id + '"><div class="player-text" id="t' + p.id + '"></div></div>');
+            p.tag = $('#i' + p.id)
+            
+            p.dataConnection.on('data', function(data) {
+                if (data.cm == 'name') {
+                  p.name = data.name;
+                  $('#t' + p.id).text(p.name);
+                  p.dataConnection.send({
+                    cm: 'headerChange',
+                    name: p.name,
+                    points: p.points,
+                    color: p.color
+                  })
+                  
+                } else if (data.cm == 'bounce') {
+                  console.log('bounce');
+                  p.tag.animate({opacity: '25%'}, function(){ p.tag.animate({opacity: '100%'})});
+                }
+              });
+            p.dataConnection.on('open', function() {
+              $.get( "start/screens/content.html", function( data ) {
+                p.dataConnection.send({
+                  cm: "contentChange",
+                  content: data
+                });
+              });
+              if (game.players.length == 1) {
+                checkvip(game);
+              } 
+              
+            });
+            p.dataConnection.on('close', function() {
+              p.tag.remove();
+              game.players.filter(item => item !== p);
+              console.log('Player Disconnected');
+              checkvip();
+            });
+            p.dataConnection.on('error', function(e) {
+             
+              console.log(e);
+              
+            });
+            game.players.push(p);
+            
+        }
+    });
 }
